374d9d68557555019c27024299a0ee6833fbc028
Ordinary56 <71382932+Ordinary56@users.noreply.github.com>
2024-10-04 10:00:33 +0200

    Created Database (#1)
    Co-authored-by: Balogh Tamás <balogh.tamas2@burg.lo.hu>

---
+++ Models/EventDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Dusza.Models
+{
+    public record class EventDetails
+    {
+        public int Id { get; set; }
+        public int EventAmount { get; set; }
+        public int ParticipantAmount { get; set; }
+
+    }
+}
