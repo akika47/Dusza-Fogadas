77564180ebf76b9646cce2c76264e1eb4a92d39e
akika47 <61090999+akika47@users.noreply.github.com>
2024-10-09 13:48:40 +0200

    Frontend (#8)
    * Mvvmfog 2 (#4)
    
    * test data added
    
    * basic template done
    
    * datagrid to listview
    
    * create event page started
    
    * some basic UI's
    
    * UI's are done basically
    
    * Gitnek a hulyesegei megoldva

--- Pages/CreateEvent.xaml.cs
+++ Pages/CreateEvent.xaml.cs
@@ -27,22 +27,11 @@ namespace WPF_Dusza.Pages
 
         private void btnCreateEvent_Click(object sender, RoutedEventArgs e)
         {
-            string eventName = txtEventName.Text;
+            string eventName = txtGameName.Text;
             string organizerName = txtOrganizerName.Text;
             string player1Name = txtPlayerNames.Text.Split('-')[0];
             string player2Name = txtPlayerNames.Text.Split('-')[1];
             int multiplier = int.Parse(txtMultiplier.Text);
-        }
-
-        private static readonly Regex _regex = new Regex("[^0-9.-]+");
-        private static bool IsTextAllowed(string text)
-        {
-            return !_regex.IsMatch(text);
-        }
-
-        private void txtMultiplier_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !IsTextAllowed(e.Text);
         }
     }
 }
