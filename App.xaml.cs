870001fcad5bea406c8c2af06e7195819dafde76
akika47 <61090999+akika47@users.noreply.github.com>
2024-10-13 22:30:18 +0200

    utsovalami (#18)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,22 +1,19 @@
 ﻿using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Crypto;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Markup;
 using WPF_Dusza.Utils;
 
-
 namespace WPF_Dusza
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         const string _connstring = "datasource=127.0.0.1;port=3306;username=root;password=;database=";
         protected override async void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);  
+            base.OnStartup(e);
             await StartAppAsync();
         }
         async Task StartAppAsync()
@@ -35,26 +32,18 @@ namespace WPF_Dusza
                     command = new(cmd, connection);
                     await command.ExecuteNonQueryAsync();
                 }
-                await LoadDataToDB(reader, command);
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 WindowUtils.DisplayErrorMessage("Hiba! Nem sikerült elérni az adatbázist!\n " +
                     $"Az alkalmazás most leáll (Hiba:{ex.Message} )");
-
                 Current.Shutdown();
             }
         }
-        async Task LoadDataToDB(StreamReader reader, MySqlCommand command)
-        {
-            reader = new(@".\Database\data.txt");
-            command.CommandText = await reader.ReadToEndAsync();
-            await command.ExecuteNonQueryAsync();
-        }
         async Task<bool> DatabaseExists(MySqlConnection connection)
         {
-            string cmd = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME=betting"; 
-            using MySqlCommand command = new(cmd,connection);
+            string cmd = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME=\'betting\'";
+            using MySqlCommand command = new(cmd, connection);
             int exists = Convert.ToInt32(await command.ExecuteScalarAsync());
             return exists > 0;
         }
