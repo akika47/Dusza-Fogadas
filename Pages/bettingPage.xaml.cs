7c80383ce4ada648fb46f8cb236d173568145906
akika47 <akika02531@gmail.com>
2024-10-13 23:04:07 +0200

    bettingBugFix

--- Pages/bettingPage.xaml.cs
+++ Pages/bettingPage.xaml.cs
@@ -25,52 +25,73 @@ namespace WPF_Dusza.Pages
 			_repo = repo;
 			_currentUser = user;
 			Loaded += async (o, e) => await FillComboboxes();
-			cbxEvents.MouseDoubleClick += SelectEvent;
 		}
 
-		public bettingPage()
-		{
-            InitializeComponent();
-        }
 
 
-        private void SelectEvent(object sender, MouseButtonEventArgs e)
+        private void SelectEvent(object sender, SelectionChangedEventArgs e)
         {
-            if(sender is ComboBoxItem item)
-			{
-				_selectedEvent = (Event)item.Content;
-			}
+            if (cbxEvents.SelectedItem is Event selectedEvent)
+            {
+                _selectedEvent = selectedEvent;
+            }
         }
-
+        private void SelectParticipant(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbxParticipants.SelectedItem is Participant selectedParticipant)
+            {
+                _selectedParticipant = selectedParticipant;
+            }
+        }
         async Task FillComboboxes()
 		{
 			List<Event> events = await _repo.EventRepository.GetEventsAsync(SelectedGame).ToListAsync();
 			events.ForEach(e => cbxEvents.Items.Add(e));
 			 SelectedGame.Participants.ForEach(x => cbxParticipants.Items.Add(x));
 		}
-		public async void PlaceBet(object sender, RoutedEventArgs e)
-		{
-			int betAmount;
-			if(!int.TryParse(txtBetAmount.Text, out betAmount))
-			{
-				WindowUtils.DisplayErrorMessage("Hiba, a pontok csak számok lehetnek");
-				return;
-			}
+        public async void PlaceBet(object sender, RoutedEventArgs e)
+        {
+            int betAmount;
+            if (!int.TryParse(txtBetAmount.Text, out betAmount))
+            {
+                WindowUtils.DisplayErrorMessage("Hiba, a pontok csak számok lehetnek");
+                return;
+            }
+
+            if (_selectedEvent == null)
+            {
+                WindowUtils.DisplayErrorMessage("Please select an event");
+                return;
+            }
+
+            if (_selectedParticipant == null)
+            {
+                WindowUtils.DisplayErrorMessage("Please select a participant");
+                return;
+            }
+
             Bet bet = new()
             {
-				EventId = _selectedEvent.Id,
-				UserID = _currentUser!.Id,
-				ParticipantId = _selectedParticipant!.Id,
-				Prediction = txtPrediction.Text,
-				BetAmount = betAmount
-			};
-			User modifiedUser = _currentUser with { Points = _currentUser.Points - betAmount};
-			await _repo.BetRepository.PlaceBetAsync(bet);
-			await _repo.UserRepository.ModifyUserAsync(modifiedUser);
-			_currentUser = modifiedUser;
-		}
+                EventId = _selectedEvent.Id,
+                UserID = _currentUser!.Id,
+                ParticipantId = _selectedParticipant!.Id,
+                Prediction = txtPrediction.Text,
+                BetAmount = betAmount
+            };
 
-		private void TextBox_MouseEnter(object sender, MouseEventArgs e)
+            User modifiedUser = _currentUser with { Points = _currentUser.Points - betAmount };
+            await _repo.BetRepository.PlaceBetAsync(bet);
+            await _repo.UserRepository.ModifyUserAsync(modifiedUser);
+            _currentUser = modifiedUser;
+
+            MessageBox.Show("Bet placed successfully!");
+
+            this.Close();
+            var eventListPage = new EventList(_repo, _currentUser);
+            eventListPage.ShowDialog();
+        }
+
+        private void TextBox_MouseEnter(object sender, MouseEventArgs e)
 		{
 			var textBox = (TextBox)sender;
 			if (textBox.ToolTip != null)
